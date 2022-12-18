ffbd1918294a2afa6463ad2b7ecfa07383dbdeb8
VukIG <vuk.ignjatovic05@gmail.com>
2022-12-17 16:09:06 -0900

    teksture mi trebaju za saturnov prsten

--- main.js
+++ main.js
@@ -14,16 +14,20 @@ renderer.setSize(window.innerWidth,window.innerHeight);
 
 camera.position.setZ(30);
 
+const lorem = new THREE.TextureLoader().load('saturn.jpg');
+
 const geometry = new THREE.SphereGeometry( 7.5, 16, 8 );
-const material = new THREE.MeshStandardMaterial( { color: 0xffff00 } );
+const material = new THREE.MeshStandardMaterial( { map: lorem } );
 const sphere = new THREE.Mesh( geometry, material );
 scene.add( sphere );
 
-const geometry3 = new THREE.TorusGeometry( 15,  3, 10, 100 );
-const material3 = new THREE.MeshStandardMaterial( { color: 0xffff00} );
-const torus = new THREE.Mesh( geometry3, material3 );
-scene.add( torus );
+const ipsum = new THREE.TextureLoader().load('ring.jpeg');
 
+const geometry2 = new THREE.RingGeometry( 10, 20, 320 );
+const material2 = new THREE.MeshStandardMaterial( { map: ipsum, side: THREE.DoubleSide } );
+const torus = new THREE.Mesh( geometry2, material2 );
+scene.add( torus );
+torus.rotation.x+=300;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -37,11 +41,12 @@ const lightHelper2= new THREE.PointLightHelper(light2);
 const gridHelper= new THREE.GridHelper(100,100,100)
 scene.add( lightHelper2,gridHelper );
 
-torus.rotation.x+=300;
+
+
 
 function anime() {
   requestAnimationFrame(anime);
-  
+  torus.rotation.z+=0.1
   sphere.rotation.y+=0.1;
   controls.update()
   renderer.render(scene,camera);  
@@ -51,13 +56,12 @@ function studzianki() {
   const geometryz = new THREE.SphereGeometry( 0.5, 24, 24 );
   const materialz = new THREE.MeshBasicMaterial( { color: 0xffffff } );
   const zvezda= new THREE.Mesh( geometryz, materialz );
-  
   const [a,b,c] = Array(3).fill().map( () => THREE.MathUtils.randFloatSpread(100) )
   zvezda.position.set(a,b,c);
   scene.add(zvezda);
 }
 
-const space= new THREE.TextureLoader().load('space.jpg')
+const space= new THREE.TextureLoader().load('space.jpg');
 scene.background = space;
 
 Array(200).fill().forEach(studzianki);
